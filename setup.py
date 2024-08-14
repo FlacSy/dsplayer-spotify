b7b25bdec5a9e8aa307f4a4d995b906f1b10539c
FlacSy <flacsy.x@gmail.com>
2024-08-14 21:44:57 +0000

    1.0.0

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dsplayer-spotify',  
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'dsplayer',
+        'yt-dlp',
+        'spotipy'
+    ],
+    entry_points={
+        'dsplayer.plugins': [
+            'youtube = plugin.plugin:SpotifyPlugin',
+        ],
+    },
+)
